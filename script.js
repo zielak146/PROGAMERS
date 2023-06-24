05cbd1ae80d20db1618455f015e564097d2c633f
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-06-24 18:22:42 +0100

    Upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+
+
+
+     function addBackground() {
+      if (window.scrollY >= 100) {
+        nav.classList.add('nav-background');
+      } else {
+        nav.classList.remove('nav-background');
+      }
+    }
+ 
+    allNavItems.forEach(item => item.addEventListener('click', () => {
+      navList.classList.remove('show')
+    }))
+
+    window.addEventListener('scroll',addBackground)
+  })
+
+//   const allNavItems = document.querySelectorAll('.nav-link');
+//   const navList = document.querySelector('.navbar-collapse');
+
+//  allNavItems.forEach(item => item.addEventListener('click' , () => {navList.classList.remove('show')}))
